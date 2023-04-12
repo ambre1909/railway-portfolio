e51600fd362ac1c33edfcee7e75a57728164d35d
akanksha ambre <aga19092001@gmail.com>
2023-04-12 16:29:20 +0530

    updated

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const cors = require("cors")
 const db = require("./config/db")
+const path = require("path")
 
 const app = express()
 require("dotenv").config({ path: "./.env" })
 db()
+app.use(express.static(path.join(__dirname, "build")))
 app.use(express.json())  //req.body madhe data aanto
 app.use(cors())
 
